468aeea1a677bade7009f3a250e79ddb9ea73433
Gregor Zurowski <gregor@zurowski.org>
2014-05-06 22:34:03 -0400

    Update metadata
    Signed-off-by: Gregor Zurowski <gregor@zurowski.org>

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'adobecq'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
-license          'All rights reserved'
-description      'Installs/Configures adobecq'
+maintainer       'Gregor Zurowski'
+maintainer_email 'gregor@zurowski.org'
+license          'Apache 2.0'
+description      'Installs and configures Adobe CQ'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
